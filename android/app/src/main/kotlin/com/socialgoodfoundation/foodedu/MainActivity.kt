64057483b0e4b87f128b5f80e5c3e76de7b41d49
TrungNguyen1208 <thanhtrungnguyen1208@gmail.com>
2021-01-19 11:21:13 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/socialgoodfoundation/foodedu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.socialgoodfoundation.foodedu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/source/general/contanst/app_theme.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color primary = Color(0xffFB6D3B);
+  static Color orange = Color(0xffFC9631);
+  static Color primaryLight = Color(0xffFFF1E5);
+
+  static Color darkGrayHighlighted = Color(0xff4F585E);
+  static Color darkBlueGray = Color(0xff697586);
+  static Color darkGray = Color(0xff9B9B9B);
+  static Color lightGray = Color(0xffC3C3C3);
+  static Color offWhite = Color(0xffF6F6F6);
+  static Color white = Colors.white;
+
+  static Color green = Color(0xff3ABC5E);
+  static Color red = Color(0xffF43E3E);
+  static Color blue = Color(0xff4B8EDD);
+}
+
+class AppFont {
+  static const largeTitle = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 40.0,
+    fontWeight: FontWeight.w600,
+    height: 1.2,
+  );
+
+  static const title1 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 27.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const title2 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 20.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const headline1 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const headline2 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 16.0,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const body1 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 16.0,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const body2 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 15.0,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const caption1 = TextStyle(
+    fontFamily: "AvenirNextLTPro",
+    fontSize: 12.0,
+    fontWeight: FontWeight.w600,
+  );
+}
